a28db05ac2004d15e691cf073052ac061c962abf
Laura Greisman <LauraGreisman@Lauras-MacBook-Air.local>
2015-03-28 19:22:18 -0700

    Initial Commit

---
+++ MRTBHearingScreening/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MRTBHearingScreening
+//
+//  Created by Laura Greisman on 3/28/15.
+//  Copyright (c) 2015 Miguel Clark. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
